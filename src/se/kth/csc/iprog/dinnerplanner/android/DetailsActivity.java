327db9681a9d164c56888f126df336a0b67a6d05
ranby <erik.ranby@gmail.com>
2014-02-06 15:54:39 +0100

    added details activity

---
+++ src/se/kth/csc/iprog/dinnerplanner/android/DetailsActivity.java
@@ -0,0 +1,18 @@
+package se.kth.csc.iprog.dinnerplanner.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
+
+/**
+ * Created by Erik on 2014-02-06.
+ */
+public class DetailsActivity  extends Activity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.details_screen);
+
+    }
+}

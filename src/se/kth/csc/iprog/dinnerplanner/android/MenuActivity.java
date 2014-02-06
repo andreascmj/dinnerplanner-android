b504ed7455cc517e2f5a60d1a8724776adfbe16d
Jonas Stendahl <jonas.stenahl@outlook.com>
2014-02-06 15:12:49 +0100

    Number of guests implemented, dropdown

--- src/se/kth/csc/iprog/dinnerplanner/android/MenuActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MenuActivity.java
@@ -2,15 +2,20 @@ package se.kth.csc.iprog.dinnerplanner.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
 
 /**
  * Created by Jonas on 2014-02-06.
  */
 public class MenuActivity extends Activity {
+
+    DinnerModel dinner = new DinnerModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +24,22 @@ public class MenuActivity extends Activity {
 
         // Set the view for the main activity screen
         // it must come before any call to findViewById method
+
         setContentView(R.layout.planning_screen);
         fillDropdown();
+        Spinner spinner = (Spinner) findViewById(R.id.dropdown);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                int selected = (int) l+1;
+                dinner.setNumberOfGuests(selected);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                dinner.setNumberOfGuests(0);
+            }
+            });
+
 
     }
 
@@ -30,4 +49,6 @@ public class MenuActivity extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
+
+
 }

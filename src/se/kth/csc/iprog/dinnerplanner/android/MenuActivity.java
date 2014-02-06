4dfa68e21a6520fdb98d648c8e534c1a14491dc6
Jonas Stendahl <jonas.stenahl@outlook.com>
2014-02-06 14:40:25 +0100

    Clickable true on start button

--- src/se/kth/csc/iprog/dinnerplanner/android/MenuActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MenuActivity.java
@@ -9,13 +9,14 @@ import se.kth.csc.iprog.dinnerplanner.android.view.ExampleView;
  * Created by Jonas on 2014-02-06.
  */
 public class MenuActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Default call to load previous state
         super.onCreate(savedInstanceState);
 
-        ExampleView mainView = new ExampleView(findViewById(R.id.planning_screen));
-
-        setContentView(R.layout.activity_main);
+        // Set the view for the main activity screen
+        // it must come before any call to findViewById method
+        setContentView(R.layout.planning_screen);
     }
 }

cd78b663b79d2d2b87b2e8f08a38ce6472933c3d
andreascmj <andreas.cm.johansson@gmail.com>
2014-02-07 13:32:11 +0100

    wat

--- src/se/kth/csc/iprog/dinnerplanner/android/MenuActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/MenuActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -116,6 +117,10 @@ public class MenuActivity extends Activity implements View.OnClickListener {
         Dish s = (Dish)view.getTag();
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle(s.getName());
+
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.popup_layout, null));
         ImageView iv = (ImageView)findViewById(R.id.popupImage);
         int imageId = getResources().getIdentifier(s.getImage(), "drawable", getPackageName());
         //iv.setImageResource(imageId);
@@ -124,7 +129,6 @@ public class MenuActivity extends Activity implements View.OnClickListener {
         TextView tv = (TextView)findViewById(R.id.popupText);
         tv.setText(s.getDescription());
 
-        //builder.setView(R.layout.popup_layout);
         AlertDialog dialog = builder.create();
         dialog.show();
     }

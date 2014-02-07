15f35b5a3d8ad505c89adc8ef435116c45f9ebb3
Jonas Stendahl <jonas.stenahl@outlook.com>
2014-02-07 14:19:23 +0100

    shit's starting to work

--- src/se/kth/csc/iprog/dinnerplanner/android/DetailsActivity.java
+++ src/se/kth/csc/iprog/dinnerplanner/android/DetailsActivity.java
@@ -3,11 +3,15 @@ package se.kth.csc.iprog.dinnerplanner.android;
 import android.app.Activity;
 import android.media.Image;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import org.apache.http.impl.conn.AbstractClientConnAdapter;
 
 import java.util.HashSet;
 
@@ -23,17 +27,20 @@ public class DetailsActivity  extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.details_screen);
+        set_details();
         set_total_cost();
         populateImageButtons();
     }
 
     public void set_details(){
         TextView details_text_box = (TextView)findViewById(R.id.details_text);
+        TextView header = (TextView) findViewById(R.id.header_text);
         String ingredients = "";
         for (Ingredient i :MenuActivity.dinner.getAllIngredients()){
             ingredients = ingredients + i.getName() + " \n";
         }
         details_text_box.setText(ingredients);
+        header.setText("Ingredients");
     }
     public void set_details_dish(Dish dish){
         TextView details_text_box = (TextView)findViewById(R.id.details_text);
@@ -41,10 +48,10 @@ public class DetailsActivity  extends Activity implements View.OnClickListener{
         TextView header_details_text_box = (TextView)findViewById(R.id.header_text);
         int type = dish.getType();
         switch(type){
-            case(1): header_details_text_box.setText("Starter");
-            case(2): header_details_text_box.setText("Main");
-            case(3): header_details_text_box.setText("Desert");
-            default: header_details_text_box.setText("Ingredients");
+            case(1): header_details_text_box.setText("Starter"); break;
+            case(2): header_details_text_box.setText("Main"); break;
+            case(3): header_details_text_box.setText("Desert"); break;
+            default: header_details_text_box.setText("Ingredients"); break;
         }
     }
 
@@ -54,10 +61,12 @@ public class DetailsActivity  extends Activity implements View.OnClickListener{
     }
 
     public void populateImageButtons() {
-        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(150, 150);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(150, 150);
         ImageButton ingredientsButton = (ImageButton) findViewById(R.id.ingredient_image);
         ingredientsButton.setImageResource(getResources().getIdentifier("ingredients", "drawable", getPackageName()));
         ingredientsButton.setLayoutParams(lp);
+        ingredientsButton.setTag("ingredients");
+        ingredientsButton.setOnClickListener(this);
 
         if(MenuActivity.dinner.getFullMenu().isEmpty()) {
 
@@ -66,20 +75,21 @@ public class DetailsActivity  extends Activity implements View.OnClickListener{
             Dish main = MenuActivity.dinner.getSelectedDish(2);
             Dish desert = MenuActivity.dinner.getSelectedDish(3);
 
+            LinearLayout ll = (LinearLayout) findViewById(R.id.details_lin_layout);
             if(starter != null) {
-                createImageButton(starter);
+                createImageButton(starter, ll);
             }
             if(main != null) {
-                createImageButton(main);
+                createImageButton(main, ll);
             }
             if(desert != null) {
-                createImageButton(desert);
+                createImageButton(desert, ll);
             }
         }
     }
 
-    private void createImageButton(Dish dish) {
-        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(150, 150);
+    private void createImageButton(Dish dish, LinearLayout ll) {
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(150, 150);
 
         ImageButton ib = new ImageButton(this);
         int imageId = getResources().getIdentifier(dish.getImage(), "drawable", getPackageName());
@@ -87,11 +97,16 @@ public class DetailsActivity  extends Activity implements View.OnClickListener{
         ib.setLayoutParams(lp);
         ib.setTag(dish);
         ib.setOnClickListener(this);
+        ll.addView(ib);
     }
 
     @Override
     public void onClick(View view) {
         ImageButton pressed = (ImageButton)view;
+        if(pressed.getTag() == "ingredients") {
+            set_details();
+            return;
+        }
         Dish details_of = (Dish)pressed.getTag();
         set_details_dish(details_of);
     }
